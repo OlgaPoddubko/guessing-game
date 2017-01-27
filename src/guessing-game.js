45683f14e0ec5f5eca96325b657ba03cf4dbc820
Olga <olgapoddubko@gmail.com>
2017-01-28 00:30:01 +0300

    guessing-game: ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	let middle = Math.ceil((this.min + this.max) / 2);
+    	return middle;
     }
 
     lower() {
-
+    	this.max = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+    	this.min = Math.ceil((this.min + this.max) / 2);
     }
 }
 
